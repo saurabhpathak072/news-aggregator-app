aa41d1f2bb9c84b91e08630c3b7aa9702416987c
Saurabh <saurabhpathak072@gmail.com>
2020-02-26 17:00:35 +0530

    Article  Author

--- src/Script/request.js
+++
@@ -1,8 +0,0 @@
-export const getData = async url => {
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error('could not fetch data');
-    }
-    const data = await res.json();
-    return data;
-  };
